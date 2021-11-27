1b646a7882688cb3c98b96cc4382d42c74a8d64e
sergeysobaka <48152999+sergeysobaka@users.noreply.github.com>
2021-11-27 23:23:37 +0300

    Add files via upload

---
+++ Джава скрипт лаба/1 лаба/1.js
@@ -0,0 +1,10 @@
+alert('Вас приветствует учебный центр')
+let name1 = prompt('Введите Ваше имя:', 'Имя')
+alert('Добро пожаловать на наши курсы, ' + name1)
+let vibor = confirm('Хотите стать Web-дизайнером?')
+if (vibor) {
+  alert('Учите стили CSS и JavaScript')
+}
+else {
+   alert('Упускаете время!')
+}
